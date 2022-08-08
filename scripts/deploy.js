805d08340239d95b68c042ec2c5a6b274a8cc01b
Azutech <azunna.onugha@gmail.com>
2022-08-08 15:04:17 +0100

    ft-deploy.js the contract has been deployed to address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,19 +6,13 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main = async () => {
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await buyMeACoffee.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
