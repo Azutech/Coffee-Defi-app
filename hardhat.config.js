7579739ccfa42f8516cac20d409a8d5e8e6bdc57
Azutech <azunna.onugha@gmail.com>
2022-08-09 08:09:09 +0100

    environment variable created

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: GEORLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
